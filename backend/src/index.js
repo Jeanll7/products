e50174d832f447c394ca353d47e926660ed571cb
devjean <jeancarloleall@gmail.com>
2023-01-31 12:16:19 -0300

    back

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,38 @@ const express = require("express");
 
 const server = express();
 
+const cors = require("cors");
+
 server.use(express.json());
 
-server.listen(3333, () => {
-  console.log("Server up! ^_^");
+server.use(cors());
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+function consultarPreco(preco) {
+  return prisma.produto.findUnique({
+    where: {
+      preco,
+    },
+  });
+}
+
+server.get("/produto", async (req, res) => {
+  const produtos = await prisma.produto.findMany();
+
+  return res.json(produtos);
+});
+
+server.get("/produto/:preco", async (req, res) => {
+  const produto = await consultarPreco(req.params.preco);
+
+  return produto
+    ? res.json(produto)
+    : res.status(500).json("produto não encontrado");
+});
+
+server.listen(4003, () => {
+  console.log("Server up!");
 });
