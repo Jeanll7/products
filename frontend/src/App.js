909a2df9de4cc7c79f43b17854f1b98e93396234
devjean <jeancarloleall@gmail.com>
2023-02-02 14:22:30 -0300

    front input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,29 @@ function App() {
   return (
     <div className="App">
       <h1>Prisma</h1>
+      <form className="container">
+        <label>
+          ID:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Name_produto:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Código:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Preço:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Data:
+          <input type="text" name="name" />
+        </label>
+        {/* <input type="submit" value="Submit" /> */}
+      </form>
     </div>
   );
 }
